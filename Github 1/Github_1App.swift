d051a8af7cb1a933d859ee22011780d7b4e7de2c
A LEELA KRISHNA REDDY <113173306+krishboy@users.noreply.github.com>
2022-12-09 21:30:38 +0530

    Initial Commit

---
+++ Github 1/Github_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Github_1App.swift
+//  Github 1
+//
+//  Created by A.Leelakrishna Reddy on 09/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Github_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Github 1UITests/Github_1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Github_1UITestsLaunchTests.swift
+//  Github 1UITests
+//
+//  Created by A.Leelakrishna Reddy on 09/12/22.
+//
+
+import XCTest
+
+final class Github_1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
